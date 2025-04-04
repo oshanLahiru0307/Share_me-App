a6f136f46b8884359d95c3272c12b83b082b1f85
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-04-04 14:04:20 +0530

    search section is added

--- frontend/src/Components/NavigationBar.jsx
+++ frontend/src/Components/NavigationBar.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Avatar } from 'antd'
+import { Avatar, Input} from 'antd'
 import { Link } from 'react-router-dom'
 import logo from '../assets/airbnb_1724634.png'
 import { HomeOutlined, UsergroupAddOutlined, MessageOutlined, BellOutlined, SlackOutlined, LaptopOutlined} from '@ant-design/icons'
 
+const { Search } = Input
+
 const NavigationBar = () => {
   return (
-    <div className='w-full flex justify-between items-center bg-gradient-to-r from-blue-50 to-blue-500  shadow-lg px-8 py-3'>
+    <div className='w-full flex justify-between items-center bg-gradient-to-r from-blue-50 to-blue-500  shadow-lg px-16 py-3'>
       <div className='flex items-center'>
         <img src={logo} alt="Logo" style={{ width: "50px", height: "50px" }} />
         <h1 className='mx-3 text-blue-500 font-semibold text-lg'>Share Me</h1>
+        <Search placeholder="Search" allowClear style={{ width: 300 }} className='' />
       </div>
       <div>
         <ul className='flex items-center space-x-12'>
