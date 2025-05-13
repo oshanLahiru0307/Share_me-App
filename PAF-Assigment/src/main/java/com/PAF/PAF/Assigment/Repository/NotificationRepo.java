c9408413adb7894a18b7731d3dffdfae8d9dc8db
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-05-13 08:06:46 +0530

    notifacation entity is created

--- PAF-Assigment/src/main/java/com/PAF/PAF/Assigment/Repository/NotificationRepo.java
+++ PAF-Assigment/src/main/java/com/PAF/PAF/Assigment/Repository/NotificationRepo.java
@@ -1,17 +1,15 @@
 package com.PAF.PAF.Assigment.Repository;
 
 import com.PAF.PAF.Assigment.Entity.NotificationEntity;
-import org.apache.el.stream.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface NotificationRepo extends MongoRepository<NotificationEntity, String> {
     List<NotificationEntity> findByUserId(String userId);
     Optional<NotificationEntity> findById(String id);
-    void deleteById(String id);
-    boolean existsById(String id);
 
 }
